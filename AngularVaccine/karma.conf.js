ffe4d92d6939119d1bce81c59dc9116841e50d50
jackl <jacklong34@gmail.com>
2021-05-21 09:50:00 -0400

    Initial structure

--- AngularVaccine/karma.conf.js
+++ AngularVaccine/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/VaccineAngular'),
+      dir: require('path').join(__dirname, './coverage/AngularVaccine'),
       subdir: '.',
       reporters: [
         { type: 'html' },
